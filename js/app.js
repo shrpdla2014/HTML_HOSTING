a959091945d4410b485ac456db8dd2a73e987f3d
shrpdla2014 <shrpdla2014@naver.com>
2023-10-19 10:57:17 +0900

    1019

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Test2.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Test1.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
